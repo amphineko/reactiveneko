20cc2d3e371e0b22a9fd92709476b25e93a6de73
amphineko <sukareki@gmail.com>
2022-01-08 21:16:29 +0000

    feat: define assetPrefix and basePath via env

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ import withOptimizedImages from 'next-optimized-images'
 
 export default withOptimizedImages({
     // NOTE: change your base path when deploying to non-root paths
-    // basePath: '',
+    assetPrefix: process.env.ASSET_PREFIX || '',
+    basePath: process.env.BASE_PATH || '',
     images: {
         disableStaticImages: true,
         loader: 'custom',
