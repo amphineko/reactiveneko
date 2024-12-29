387e2ef0b9c3925d666d8145329d0606106003b3
amphineko <sukareki@gmail.com>
2024-12-29 11:50:19 +0000

    feat(header): stack-like primary and alt names
    ghstack-source-id: f204fa23ee55688832111cde8432c52c286e6e70
    Pull Request resolved: https://github.com/amphineko/reactiveneko/pull/24

--- src/components/display/header.tsx
+++ src/components/display/header.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren, ReactNode } from 'react'
 import { BORDER_RADIUS, BORDER_RADIUS_SMALL } from '../../lib/css'
+import { classnames } from '../../lib/classnames'
 
 export const ProfileNameStandout = ({
     backgroundColor,
@@ -35,17 +36,6 @@ export const ProfileNameStandout = ({
                     transition-duration: 0.5s;
                 }
 
-                .standout::after {
-                    color: #eee;
-                    content: '${ruby || ''}';
-                    display: block;
-                    font-size: 0.5em;
-                    line-height: 1.5em;
-                    position: absolute;
-                    top: 2.5em;
-                    transition-duration: 0.5s;
-                }
-
                 .standout:hover {
                     background: ${hoverColor};
                     box-shadow: 0 0 0.2em 0.2em rgba(0, 0, 0, 0.1);
@@ -58,47 +48,130 @@ export const ProfileNameStandout = ({
                     font-size: 0.5em;
                     position: absolute;
                     text-shadow: 0 0 0.25em rgba(0, 0, 0, 0.25);
+                    transition-duration: 0.5s;
+                }
+
+                .standout:hover::after {
+                    color: #eee;
+                    content: '${ruby || ''}';
+                    display: block;
+                    font-size: 0.5em;
+                    line-height: 1.5em;
+                    position: absolute;
+                    top: 2.5em;
+                    transition-duration: 0.5s;
                 }
             `}</style>
         </a>
     )
 }
 
-export const ProfileName = ({ children }: PropsWithChildren) => (
-    <div className="profile-name">
-        <h1 className="content">{children}</h1>
-        <span className="description">common-name</span>
-        <style jsx>{`
-            .profile-name {
-                background: #555;
-                border-radius: ${BORDER_RADIUS};
-                color: white;
-                display: block;
-                padding: 1.75em 0.5rem 0.5em 0.5rem; // NOTE: line-height direction uses em to align with font-size
-                position: relative;
-            }
+export function ProfileName({
+    children,
+    isAlt,
+    padRightCharacters,
+    zIndex,
+}: PropsWithChildren<{
+    isAlt?: boolean
+    padRightCharacters?: number
+    zIndex?: number
+}>) {
+    const padRight = padRightCharacters ? '\\00a0'.repeat(padRightCharacters) : ''
+    console.log(children, padRight)
+    return (
+        <div className={classnames({ 'profile-name': true, alt: isAlt })}>
+            {!isAlt ? <h1 className="content">{children}</h1> : <span className="content">{children}</span>}
+            <span className="description">{!isAlt ? 'common-name' : 'also-known-as'}</span>
+            <style jsx>{`
+                .profile-name {
+                    background: #555;
+                    border-radius: ${BORDER_RADIUS};
+                    box-shadow: 0 0 0.25em 0.25em rgba(0, 0, 0, 0.1);
+                    color: white;
+                    font-family: 'IBM Plex Mono', 'Courier New', monospace;
+                }
 
-            .content {
-                font-family: 'Courier New';
-                font-size: 2.5em;
-                font-weight: normal;
-                line-height: 1em;
-                margin: 0;
-                padding: 0;
-                text-wrap: none;
-            }
+                .profile-name {
+                    display: flex;
+                    flex-direction: row;
+                    flex-wrap: wrap-reverse;
+                    justify-content: space-between;
+                    min-height: 4em;
 
-            .description {
-                color: #999;
-                font-size: 1.25rem;
-                font-weight: 300;
-                position: absolute;
-                top: 0;
-                right: 0.5rem;
-            }
-        `}</style>
-    </div>
-)
+                    padding: 0.5em 0.5rem 0.5em 0.5rem; // NOTE: line-height direction uses em to align with font-size
+                    z-index: ${zIndex || 'unset'};
+                }
+
+                .content {
+                    align-self: end;
+                    font-size: 2.5em;
+                    font-weight: 700;
+                    line-height: 1em;
+                    margin: 0;
+                    padding: 0;
+                    text-wrap: none;
+                }
+
+                .content::after {
+                    content: '${padRight}';
+                    white-space: pre;
+                }
+
+                .description {
+                    align-self: flex-end;
+                    color: #999;
+                    font-size: 1.25rem;
+                    font-weight: 300;
+                }
+
+                .alt {
+                    background: #777;
+                    margin-top: -1em;
+                    padding-top: 1em;
+                    min-height: unset;
+                }
+
+                .alt .content {
+                    color: #aaa;
+                    font-size: 1.5em;
+                    font-weight: 300;
+                }
+
+                .alt .description {
+                    bottom: 0;
+                    top: auto;
+                }
+            `}</style>
+        </div>
+    )
+}
+
+export function ProfileNames({
+    alsoKnownAs,
+    children,
+}: PropsWithChildren<{
+    alsoKnownAs?: string[]
+}>) {
+    const maxLength = Math.max(...(alsoKnownAs?.map((name) => name.length) || [0]))
+    const maxZIndex = alsoKnownAs?.length || 0
+    return (
+        <div className="profile-names">
+            <ProfileName zIndex={maxZIndex + 1}>{children}</ProfileName>
+            {alsoKnownAs?.map((name, index) => (
+                <ProfileName isAlt key={name} padRightCharacters={maxLength - name.length} zIndex={maxZIndex - index}>
+                    {name}
+                </ProfileName>
+            ))}
+            <style jsx>{`
+                .profile-names {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 0.5rem;
+                }
+            `}</style>
+        </div>
+    )
+}
 
 export const ProfileAddonGroup = ({
     children,
@@ -190,11 +263,13 @@ const ProfilePicture = ({ avif, jpeg, png, webp }: ProfilePictureSources) => (
 )
 
 export const Header = ({
+    alsoKnwonAs,
     background,
     children,
     profileName,
     profilePicture,
 }: PropsWithChildren<{
+    alsoKnwonAs?: string[]
     background?: string
     profileName: ReactNode
     profilePicture: ProfilePictureSources
@@ -207,8 +282,8 @@ export const Header = ({
         </div>
 
         <div className="column column-names">
-            <ProfileName>{profileName}</ProfileName>
-            {children}
+            <ProfileNames alsoKnownAs={alsoKnwonAs}>{profileName}</ProfileNames>
+            <div className="rest-container">{children}</div>
         </div>
 
         <style jsx>{`
@@ -241,12 +316,15 @@ export const Header = ({
             }
 
             .column-names {
+                flex-basis: 65%;
+            }
+
+            .column-names .rest-container {
                 ${background ? `background: url(${background});` : ''}
                 background-attachment: fixed;
                 background-color: #f5f5f5;
                 border-radius: ${BORDER_RADIUS};
                 box-shadow: 0 0 0.25rem 0.25rem rgba(0, 0, 0, 0.1);
-                flex-basis: 65%;
                 padding: 1rem;
             }
 
